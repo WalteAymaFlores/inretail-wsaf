d9cb2465ff584439aeee7894f891bc1ceeb64b82
WalteAymaFlores <59937759+WalteAymaFlores@users.noreply.github.com>
2021-02-12 09:35:24 -0500

    Update IClienteServiceImplTest.java

--- 2.Codigo/src/test/java/intercorpretail/reto/inretail/service/IClienteServiceImplTest.java
+++ 2.Codigo/src/test/java/intercorpretail/reto/inretail/service/IClienteServiceImplTest.java
@@ -58,35 +58,5 @@ public class IClienteServiceImplTest {
         Double resultadoEsperado=11.239810941470502;        
         Assertions.assertEquals(resultadoEsperado,desviacionEstandar);
     }    
-//    @Test
-//    public void getAgeAverageClient(){
-//        IClienteServiceImpl clienteService = new IClienteServiceImpl();
-//        List<Cliente> clientes = new LinkedList<Cliente>();
-//        Cliente cliente1= new Cliente();
-//        cliente1.setNombre("Walter");
-//        cliente1.setApellido("Ayma Flores");
-//        cliente1.setEdad(27);
-//        cliente1.setFechaNacimiento(new Date("11/07/1993"));
-//        
-//        Cliente cliente2= new Cliente();
-//        cliente2.setNombre("Tito");
-//        cliente2.setApellido("Gomez Gomez");
-//        cliente2.setEdad(20);
-//        cliente2.setFechaNacimiento(new Date("11/07/2000"));
-//        
-//        Cliente cliente3= new Cliente();
-//        cliente3.setNombre("Pepe");
-//        cliente3.setApellido("Perez perez");
-//        cliente3.setEdad(5);
-//        cliente3.setFechaNacimiento(new Date("11/07/2015"));
-//        
-//        clientes.add(cliente1);
-//        clientes.add(cliente2);
-//        clientes.add(cliente3);
-//        
-//        ClienteDato clienteDato=clienteService.getAgeAverageClient(clientes);
-//        Double resultadoEsperado=clienteDato.getPromedioEdad();
-//        System.out.println("resultadoEsperado = " + resultadoEsperado);
-//        Assertions.assertEquals(resultadoEsperado, 17.33);
-//    }
+
 }
