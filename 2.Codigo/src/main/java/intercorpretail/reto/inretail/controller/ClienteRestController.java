0031608b542756567d90355ddbe84368e1bd0140
Walter Ayma Flores <walterayma22@gmail.com>
2021-02-16 01:29:42 -0500

    v5.1

--- 2.Codigo/src/main/java/intercorpretail/reto/inretail/controller/ClienteRestController.java
+++ 2.Codigo/src/main/java/intercorpretail/reto/inretail/controller/ClienteRestController.java
@@ -21,6 +21,12 @@ import intercorpretail.reto.inretail.entities.Cliente;
 import intercorpretail.reto.inretail.entities.ClienteDato;
 import intercorpretail.reto.inretail.service.IClienteServiceImpl;
 import intercorpretail.reto.inretail.service.IClienteService;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 
 /**
  *
@@ -34,19 +40,71 @@ public class ClienteRestController {
     IClienteService clienteInterface;
 
     @GetMapping("/clientes")
-    public List<Cliente> getClient() {        
-        return clienteInterface.getClient();
+    public ResponseEntity<?> getClient() {
+        List<Cliente> clientes = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            clientes = clienteInterface.getClient();
+        } catch (DataAccessException e) {
+            response.put("success", false);
+            response.put("message", "Error al consultar la base de datos");
+            response.put("data", null);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("success", true);
+        response.put("message", "Se consulto correctamete la base de datos.");
+        response.put("data", clientes);
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 
     @GetMapping("/kpideclientes")
-    public ClienteDato getAverageAndStandarDeviationClient() {
-        ClienteDato clientes = clienteInterface.getDataClient();
-        return clientes;
+    public ResponseEntity<?> getAverageAndStandarDeviationClient() {
+        ClienteDato clienteDato = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            clienteDato = clienteInterface.getDataClient();
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("message", "Error al consultar la información");
+            response.put("data", null);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("success", true);
+        response.put("message", "Se consulto correctamente la información.");
+        response.put("data", clienteDato);
+        return new ResponseEntity(response, HttpStatus.OK);        
     }
 
     @PostMapping("/creacliente")
-    public ResponseEntity<Cliente> saveClient(@RequestBody Cliente cliente) {
-        return ResponseEntity.ok(clienteInterface.saveCliente(cliente));
+    public ResponseEntity<?> saveClient(@RequestBody Cliente cliente) {
+        Cliente clientNew = null;
+        Map<String, Object> response = new HashMap<>();
+        int ageClient = cliente.getEdad();
+        Date birthDate = cliente.getFechaNacimiento();
+        int ageClientBirthDate = clienteInterface.getAge(new SimpleDateFormat("dd/MM/yyyy").format(birthDate));
+        try {
+            if (ageClient > 100 || ageClient < 0) {
+                response.put("success", false);
+                response.put("message", "La edad del cliente no es correcta");
+                response.put("data", null);
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+            } else if (ageClientBirthDate < 0 || ageClientBirthDate > 100) {
+                response.put("success", false);
+                response.put("message", "La fecha de nacimiento del cliente no es correcta");
+                response.put("data", null);
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+            }
+            clientNew = clienteInterface.saveCliente(cliente);
+        } catch (DataAccessException e) {
+            response.put("success", false);
+            response.put("message", "Error al insertar en la base de datos");
+            response.put("data", null);
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("success", true);
+        response.put("message", "El cliente ha sido creado con exito");
+        response.put("data", clientNew);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
 }
